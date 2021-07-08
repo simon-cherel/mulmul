c97c300e3db0bfa9ab45d28b1fb3970074727aa8
simon-cherel <simon.cherel@gmail.com>
2021-07-08 15:56:17 +0200

    scrolling widget revisité pr snapping

--- lib/panier.dart
+++ lib/panier.dart
@@ -1,5 +1,6 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:snapping_sheet/snapping_sheet.dart';
 
 class panier extends StatefulWidget {
   const panier({Key? key}) : super(key: key);
@@ -11,19 +12,32 @@ class panier extends StatefulWidget {
 class _panierState extends State<panier> {
   @override
   Widget build(BuildContext context) {
-    return DraggableScrollableSheet(
-      initialChildSize: 0.1,
-      maxChildSize: 0.9,
-      minChildSize: 0.1,
-      builder: (context, controller) => Container(
-        decoration: BoxDecoration(
-          color: AdaptiveTheme.of(context).theme.primaryColor,
-          borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+    return SnappingSheet(
+      //lockOverflowDrag: true,
+      grabbingHeight: 800,
+      grabbing: SizedBox.expand(
+        child: Container(
+          decoration: BoxDecoration(
+            color: AdaptiveTheme.of(context).theme.primaryColor,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(50), topRight: Radius.circular(50)),
+          ),
         ),
-        child: SingleChildScrollView(
-          controller: controller,
-          child: Container(),
+      ),
+      snappingPositions: [
+        SnappingPosition.factor(
+            positionFactor: -0.3,
+            snappingCurve: Curves.ease,
+            snappingDuration: Duration(milliseconds: 500)),
+        SnappingPosition.factor(
+            positionFactor: 0.47,
+            snappingCurve: Curves.ease,
+            snappingDuration: Duration(milliseconds: 500)),
+      ],
+      sheetBelow: SnappingSheetContent(
+        child: Container(
+          color: AdaptiveTheme.of(context).theme.primaryColor,
+          child: SingleChildScrollView(),
         ),
       ),
     );
